0867da3983c9eec8b8499aedbd929be77d449a8f
Nataly Charari <ncastrillonc@unal.edu.co>
2015-05-12 15:55:03 -0500

    Prueba Nata5

--- classes/user.php
+++ classes/user.php
@@ -10,6 +10,8 @@ class user extends object_standard
 	protected $type;
 	protected $correillo;
 	protected $ciudadania;
+        protected $losmasoridinarios;
+	
         
 	//components
 	var $components = array();
